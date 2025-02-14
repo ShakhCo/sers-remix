dc72227ce45063d0ce6a50408b644dad4617924f
ShakhCo <ssharipov3@students.wiut.uz>
2025-02-14 15:17:44 +0500

    Google tag

--- app/root.tsx
+++ app/root.tsx
@@ -1,49 +1,59 @@
 import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
+    Links,
+    Meta,
+    Outlet,
+    Scripts,
+    ScrollRestoration,
 } from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/node";
-
+import type {LinksFunction} from "@remix-run/node";
 import "./tailwind.css";
 
 export const links: LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
+    {rel: "preconnect", href: "https://fonts.googleapis.com"},
+    {
+        rel: "preconnect",
+        href: "https://fonts.gstatic.com",
+        crossOrigin: "anonymous",
+    },
+    {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    },
 ];
 
-import { Providers } from "./providers";
+import {Providers} from "./providers";
 
-export function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body className="bg-orange-50">
+export function Layout({children}: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
+        <head>
+            <meta charSet="utf-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <Meta/>
+            <Links/>
+            <script async src={`https://www.googletagmanager.com/gtag/js?id=${'G-8DCH5HXP9T'}`}/>
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-8DCH5HXP9T');
+            `,
+                }}
+            />
+        </head>
+        <body className="bg-orange-50">
         <Providers>
-          {children}
+            {children}
         </Providers>
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  );
+        <ScrollRestoration/>
+        <Scripts/>
+        </body>
+        </html>
+    );
 }
 
 export default function App() {
-  return <Outlet />;
+    return <Outlet/>;
 }
